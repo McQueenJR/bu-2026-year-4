a7e930f5149d2678231bc038c3b98b5b77da5e45
Yuutinyo <yuunapat.7482@gmail.com>
2026-08-14 23:10:15 +0700

    Revert "NPC Dialog Green(have bug)"
    This reverts commit b38f414278d62ebdb4d0e6aaec8b7c278f54b919.

--- Assets/Scripts/DialogManager.cs
+++ Assets/Scripts/DialogManager.cs
@@ -13,20 +13,8 @@ public class DialogManager : MonoBehaviour
     private string[] dialogs;
     private int currentIndex;
 
-    private enum DialogType
-    {
-        Normal,
-        Green,
-        Emergency,
-        Simple
-    }
-
-    private DialogType currentDialogType;
-
     public void StartDialog(NPCData data)
     {
-        currentDialogType = DialogType.Normal;
-
         dialogs = data.dialogs;
         currentIndex = 0;
 
@@ -36,59 +24,12 @@ public class DialogManager : MonoBehaviour
 
         ShowCurrentDialog();
     }
-
-    public void StartGreenDialog(NPCData data)
-    {
-        currentDialogType = DialogType.Green;
-
-        dialogs = data.greenDialogs;
-        currentIndex = 0;
-
-        nameText.text = data.npcName;
-
-        dialogPanel.SetActive(true);
-
-        ShowCurrentDialog();
-    }
-
-    public void StartEmergencyDialog(NPCData data)
-    {
-        if (data == null)
-        {
-            Debug.LogError("ไม่มี NPCData");
-            return;
-        }
-
-        if (data.emergencyDialogs == null ||
-            data.emergencyDialogs.Length == 0)
-        {
-            Debug.LogWarning(
-                "NPC " + data.npcName +
-                " ไม่มี Emergency Dialog"
-            );
-
-            return;
-        }
-
-        dialogs = data.emergencyDialogs;
-        currentIndex = 0;
-
-        nameText.text = data.npcName;
-
-        dialogPanel.SetActive(true);
-
-        ShowCurrentDialog();
-    }
-
     public bool IsDialogOpen()
     {
         return dialogPanel.activeSelf;
     }
-
     public void StartSimpleDialog(string speaker, string[] messages)
     {
-        currentDialogType = DialogType.Simple;
-
         dialogs = messages;
         currentIndex = 0;
 
@@ -98,6 +39,7 @@ public class DialogManager : MonoBehaviour
 
         ShowCurrentDialog();
     }
+    
 
     public void NextDialog()
     {
@@ -124,17 +66,6 @@ public class DialogManager : MonoBehaviour
     {
         dialogPanel.SetActive(false);
 
-        if (currentDialogType == DialogType.Normal)
-        {
-            GameManager.Instance.DialogFinished();
-        }
-        else if (currentDialogType == DialogType.Green)
-        {
-            GameManager.Instance.GreenDialogFinished();
-        }
-        else if (currentDialogType == DialogType.Emergency)
-        {
-            //GameManager.Instance.EmergencyDialogFinished();
-        }
+        GameManager.Instance.DialogFinished();
     }
 }
