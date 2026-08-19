1635bbbaaeac5087aca92c2570d7b1525c2b6d73
WatTunTo <nipat523naruto@gmail.com>
2026-08-19 19:12:22 +0700

    แก้บัตรไม่ตรงดาต้า

--- Assets/Scripts/IDCard/IDCardDisplay.cs
+++ Assets/Scripts/IDCard/IDCardDisplay.cs
@@ -2,10 +2,34 @@ using UnityEngine;
 
 public class IDCardDisplay : MonoBehaviour
 {
-    public GameObject displayPrefab;
-
     public void ShowCard()
     {
-        IDCardPopup.Instance.Show(displayPrefab);
+        if (GameManager.Instance.currentNPC == null)
+        {
+            Debug.LogError("ไม่มี currentNPC ตอนนี้");
+            return;
+        }
+
+        NPC npc = GameManager.Instance.currentNPC.GetComponent<NPC>();
+
+        if (npc == null)
+        {
+            Debug.LogError("NPC ไม่มี NPC.cs");
+            return;
+        }
+
+        if (npc.data == null)
+        {
+            Debug.LogError("NPC ไม่มี NPCData");
+            return;
+        }
+
+        if (npc.data.idCardDisplayPrefab == null)
+        {
+            Debug.LogError("NPC " + npc.data.npcName + " ไม่ได้ใส่ idCardDisplayPrefab");
+            return;
+        }
+
+        IDCardPopup.Instance.Show(npc.data.idCardDisplayPrefab);
     }
 }
